e31eedc7a1b0e6e9adf87d48f6d49fcb232ad485
Darren Jack <darrencharlesjack79@gmail.com>
2015-07-06 13:09:19 -0600

    added some backend scripts

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<?php include_once("home.html"); ?>
+<?php 
+	
+	include_once("home.html"); 
+	
+
+?>
--- js/init.js
+++ js/init.js
@@ -7,6 +7,27 @@
     $('.modal-trigger').leanModal();
 
     // jQuery Validate event handler
-    $('#commentForm').validate();
+    $('#commentForm').validate({
+    	submitHandler: function(form) {
+    	    $(form).ajaxSubmit({
+    	        type:"POST",
+    	        data: $(form).serialize(),
+    	        url:"process.php",
+    	        success: function() {
+    	            $('#commentForm :input').attr('disabled', 'disabled');
+    	            $('#commentForm').fadeTo( "slow", 0.15, function() {
+    	                $(this).find(':input').attr('disabled', 'disabled');
+    	                $(this).find('label').css('cursor','default');
+    	                $('#success').fadeIn();
+    	            });
+    	        },
+    	        error: function() {
+    	            $('#commentForm').fadeTo( "slow", 0.15, function() {
+    	                $('#error').fadeIn();
+    	            });
+    	        }
+    	    });
+    	}
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
