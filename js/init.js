72e09d3e9a4f9ad95c3a18b45325d66a40ec4eb4
Darren Jack <darrencharlesjack79@gmail.com>
2015-07-06 10:25:56 -0600

    organiztion

--- js/init.js
+++ js/init.js
@@ -1,9 +1,12 @@
 (function($){
   $(function(){
 
+  	// Materialize event handlers
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.modal-trigger').leanModal();
+
+    // jQuery Validate event handler
     $('#commentForm').validate();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
