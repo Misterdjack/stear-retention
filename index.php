0705ee3847daa451bcabc14f30acd5f11d8aab15
Darren Jack <darrencharlesjack79@gmail.com>
2015-07-23 15:37:10 -0600

    removed unnecessary files

--- index.php
+++ index.php
@@ -2,21 +2,4 @@
 	
 	include_once("home.html"); 
 
-	// // form handler
-	// $to = "darrenjack79@yahoo.com"; 
-	// $from = $_REQUEST['email']; 
-	// $name = $_REQUEST['name']; 
-	// $headers = "From: $from"; 
-	// $subject = "You have a message sent from your site"; 
-
-	// $fields = array(); 
-	// $fields{"name"} = "name"; 
-	// $fields{"email"} = "email"; 
-	// $fields{"phone"} = "phone"; 
-	// $fields{"message"} = "message";
-
-	// $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
-
-	// $send = mail($to, $subject, $body, $headers);
-
 ?>
